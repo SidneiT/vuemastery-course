c4a05a6932f2795052d112347c21d0f2f6ddf7dd
Sidnei Teixeira <sidnei.teixeira@telefonica.com>
2020-05-01 01:43:28 -0300

    Token base authentication Lesson 4 - User login Lesson 5 - User logout Lesson 6 - Handlig errors Lesson 7 - Automatic login

--- token-based-authentication/src/vuex/store.js
+++ token-based-authentication/src/vuex/store.js
@@ -13,15 +13,34 @@ export default new Vuex.Store({
       state.user = userData
       localStorage.setItem('user', JSON.stringify(userData))
       axios.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`
+    },
+    CLEAR_USER_DATA (state) {
+      localStorage.removeItem('user')
+      location.reload()
     }
   },
   actions: {
+    login ({ commit }, credentials) {
+      return axios
+        .post(`//localhost:3000/login`, credentials)
+        .then(({ data }) => {
+          commit('SET_USER_DATA', data)
+        })
+    },
     register ({ commit }, credentials) {
       return axios
         .post('//localhost:3000/register', credentials)
         .then(({ data }) => {
           commit('SET_USER_DATA', data)
         })
+    },
+    logout ({ commit }) {
+      commit('CLEAR_USER_DATA')
+    }
+  },
+  getters: {
+    loggedIn: state => {
+      return !!state.user
     }
   }
 })
